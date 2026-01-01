bce97a8203002ce0fbc2f91527c73f9a713485a5
User365www <96082272+User365www@users.noreply.github.com>
2026-01-01 22:59:16 +0300

    Update task2.sql

--- task2.sql
+++ task2.sql
@@ -2,17 +2,17 @@ SELECT
     a.actor_id,
     a.first_name,
     a.last_name,
-    SUM(film_rentals.rental_count) as total_rentals
-FROM actor a
-JOIN film_actor fa ON a.actor_id = fa.actor_id
-JOIN (
-    SELECT 
-        i.film_id,
-        COUNT(*) as rental_count
-    FROM rental r
-    JOIN inventory i ON r.inventory_id = i.inventory_id
-    GROUP BY i.film_id
-) film_rentals ON fa.film_id = film_rentals.film_id
-GROUP BY a.actor_id
-ORDER BY total_rentals DESC
+    COUNT(r.rental_id) AS total_rentals
+FROM 
+    actor a
+    JOIN film_actor fa ON a.actor_id = fa.actor_id
+    JOIN film f ON fa.film_id = f.film_id
+    JOIN inventory i ON f.film_id = i.film_id
+    JOIN rental r ON i.inventory_id = r.inventory_id
+GROUP BY 
+    a.actor_id, 
+    a.first_name, 
+    a.last_name
+ORDER BY 
+    total_rentals DESC
 LIMIT 10;
