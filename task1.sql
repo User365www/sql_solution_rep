d414e7b92a2d1cd3ef0995d111641f02f86ee3ce
User365www <96082272+User365www@users.noreply.github.com>
2026-01-01 22:40:11 +0300

    Update task1.sql

--- task1.sql
+++ task1.sql
@@ -1,5 +1,5 @@
 EXPLAIN ANALYZE
-SELECT COUNT(*) as count_f, c.name
+SELECT COUNT(c.name) as count_f, c.name
 FROM film_category fc
 JOIN category c ON c.category_id = fc.category_id
 GROUP BY c.name
