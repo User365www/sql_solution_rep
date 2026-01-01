396fc2eb67e753166f155ee9b9071485d7c051a7
User365www <96082272+User365www@users.noreply.github.com>
2026-01-01 23:03:53 +0300

    Update task3.sql

--- task3.sql
+++ task3.sql
@@ -1,18 +1,9 @@
-WITH category_payments AS (
-    SELECT 
-        fc.category_id,
-        p.amount
-    FROM payment p
-    JOIN rental r ON p.rental_id = r.rental_id
-    JOIN inventory i ON r.inventory_id = i.inventory_id
-    JOIN film_category fc ON i.film_id = fc.film_id
-)
-SELECT 
-    c.category_id,
-    c.name,
-    SUM(cp.amount) as sum_spent
+SELECT c.category_id, c.name, SUM(p.amount) as sum_spent
 FROM category c
-JOIN category_payments cp ON c.category_id = cp.category_id
-GROUP BY c.category_id, c.name
+JOIN film_category fc ON c.category_id = fc.category_id
+JOIN inventory i ON fc.film_id = i.film_id
+JOIN rental r ON r.inventory_id = i.inventory_id
+JOIN payment p ON p.rental_id = r.rental_id
+GROUP BY c.category_id
 ORDER BY sum_spent DESC
 LIMIT 1;
